469c2ce7017366676cd0cf14d4e8bfd3a00fdb58
davidfariasd96 <fariasdiaz96@outlook.com>
2023-05-22 23:39:16 -0400

    Se agrega buscar categoria por id

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -9,7 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService{
@@ -33,6 +35,39 @@ public class CategoryServiceImpl implements ICategoryService{
         }catch(Exception ex){
 
             response.setMetadata("No ok", "-1","Error al consultar");
+            ex.getStackTrace();
+            return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+
+        CategoryResponseRest response = new CategoryResponseRest();
+        List<Category> list = new ArrayList<>();
+
+        try{
+
+            Optional<Category> category = categoryDao.findById(id);
+
+            if (category.isPresent()){
+
+                list.add(category.get());
+                response.getCategoryResponse().setCategory(list);
+                response.setMetadata("Respuesta ok", "00","Categoria encontrada");
+
+            }else{
+
+                response.setMetadata("No ok", "-1","Categoria no encontrada");
+                return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.NOT_FOUND);
+            }
+
+        }catch(Exception ex){
+
+            response.setMetadata("No ok", "-1","Error al consultar por id");
             ex.getStackTrace();
             return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
         }
